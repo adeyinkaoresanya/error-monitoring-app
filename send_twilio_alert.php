7e26799f1ca0ef06a44e515622686e7b3d79bfe8
AdeyinkaOresanya <adeyinkaoresanya@gmail.com>
2024-08-08 12:07:05 +0100

    update send_twilio_alert

--- send_twilio_alert.php
+++ send_twilio_alert.php
@@ -9,10 +9,16 @@ $dotenv->load();
 
 use Twilio\Rest\Client;
 
-$twilio_sid = $_ENV['TWILIO_ACCOUNT_SID'];
-$twilio_token = $_ENV['TWILIO_AUTH_TOKEN'];
-$twilio_number = $_ENV['TWILIO_PHONE_NUMBER'];
-$alert_number = $_ENV['ALERT_PHONE_NUMBER'];
+// $twilio_sid = $_ENV['TWILIO_ACCOUNT_SID'];
+// $twilio_token = $_ENV['TWILIO_AUTH_TOKEN'];
+// $twilio_number = $_ENV['TWILIO_PHONE_NUMBER'];
+// $alert_number = $_ENV['ALERT_PHONE_NUMBER'];
+
+$twilio_sid = getenv('TWILIO_ACCOUNT_SID');
+$twilio_token = getenv('TWILIO_AUTH_TOKEN');
+$twilio_number = getenv('TWILIO_PHONE_NUMBER');
+$alert_number = getenv('ALERT_PHONE_NUMBER');
+
 
 $client = new Client($twilio_sid, $twilio_token);
 $error_message = "Critical error detected in your application. Please check the logs immediately.";
